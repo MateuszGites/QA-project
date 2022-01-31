6046af9be99aa4e8c5f013521b54a2f71a78534a
MateuszGites <mateusz97bieniek@gmail.com>
2022-01-31 17:11:01 +0000

    please work

---
+++ src/test/java/com/qa/user_app/QaProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.user_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaProjectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
